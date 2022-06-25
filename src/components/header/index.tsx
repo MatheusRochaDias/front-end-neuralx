f27b8a638f39064c65c09fdc29f1d70be1ad84fb
Allisson-Lima-dev <Allysoncosta@outlook.com>
2022-06-25 10:41:35 -0300

    refactor: Adicionando header responsivo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,27 @@
 import React from "react";
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { HiOutlineMenuAlt2 } from "react-icons/hi";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Icon,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { Layout } from "~/components";
 import Router from "next/router";
 import Link from "next/link";
@@ -7,6 +29,8 @@ import Logo from "/public/assets/Neural.png";
 
 export function Header() {
   const asPath = Router.asPath;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef(null);
   const routesPath = [
     {
       title: "Home",
@@ -29,6 +53,7 @@ export function Header() {
       path: "/contact",
     },
   ];
+  const scrollY = window.scrollY;
   return (
     <Flex
       // w="full"
@@ -38,11 +63,23 @@ export function Header() {
       mx="auto"
       w={{ base: "95%", md: "90%", lg: "85%" }}
       py="30px"
+      // pos={"fixed"}
+      // // top="0px"
+      // right={0}
+      // left={0}
+      // zIndex={1500}
+      // bg={scrollY > 0 ? "#150433a0" : "transparent"}
     >
       <Box>
-        <Image src={Logo.src} w="100px" h="120px" alt="Logo do neuralX" />
+        <Image
+          src={Logo.src}
+          w={{ base: "60px", lg: "80px", xl: "100px" }}
+          h={{ base: "60px", lg: "90px", xl: "120px" }}
+          alt="Logo do neuralX"
+        />
       </Box>
       <Flex
+        display={{ base: "none", lg: "flex" }}
         color={"#fff"}
         justify="space-between"
         align={"center"}
@@ -67,6 +104,47 @@ export function Header() {
           </Link>
         ))}
       </Flex>
+      <Box display={{ base: "flex", lg: "none" }} zIndex={1000}>
+        <HiOutlineMenuAlt2 onClick={onOpen} size={30} color="#ffff" />
+      </Box>
+
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent bg={"#150433"} color="#fff">
+          <DrawerCloseButton />
+          <DrawerHeader alignItems={"center"}>
+            <Flex align={"center"}>
+              <Image src={Logo.src} alt="Icone Logo" w="80px" h="60px" />
+            </Flex>
+          </DrawerHeader>
+
+          <DrawerBody>
+            {routesPath.map((item, index) => (
+              <Flex
+                align="center"
+                // w="80%"
+                p="8px"
+                borderRadius={"5px"}
+                my="5px"
+                key={index}
+                color={item.path === asPath ? "#ffffff" : "#fff"}
+                bg={item.path === asPath ? "#5E239D" : "transparent"}
+              >
+                <Box ml="10px" fontSize={"18px"}>
+                  <Link href={item.path}>{item.title}</Link>
+                </Box>
+              </Flex>
+            ))}
+          </DrawerBody>
+
+          <DrawerFooter></DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </Flex>
   );
 }
