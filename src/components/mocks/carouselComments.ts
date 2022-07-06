f0b317c7c30c4d6ecd3eee858114accb474467aa
Allisson-Lima-dev <Allysoncosta@outlook.com>
2022-07-06 17:50:25 -0300

    feat: adicionando mock para comment do carousel

---
+++ src/components/mocks/carouselComments.ts
@@ -0,0 +1,35 @@
+import Ia from "../../../public/assets/IA.png";
+import Img from "../../../public/assets/imgCarousel.png";
+import User from "../../../public/assets/user.png";
+import User2 from "../../../public/assets/user2.png";
+import User3 from "../../../public/assets/user3.png";
+
+export const dataComments = [
+  {
+    image: User.src,
+    author: "Jana Santos - UI/Ux",
+    comment:
+      "Id consectetur purus ut faucibus pulvinar elementum integer enim. A arcu cursus vitae congue mauris rhoncus aeneanconsectetur purus ut faucibus ",
+  },
+  {
+    image:
+      "https://s3.amazonaws.com/www-inside-design/uploads/2019/03/featureuserpersona.jpg",
+    author: "Allisson Lima - Developer",
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ad tempore consequuntur nihil reprehenderit aliquam minus ",
+  },
+  {
+    image:
+      "https://content.codecademy.com/programs/ui-design/user-personas/pexels-photo-428339.jpg",
+    author: "Luiz Felipe - Empresario",
+    comment:
+      "Magna sit amet purus gravida. Sit amet porttitor eget dolor morbi nonLectus vestibulum mattis ullamcorper velit sed ullamcorper.Diam phasellus .",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZdLphXncQObV6aZlZHHE3Jj1SIwX6_sBZc2x1P0zvpOYxJmr-Pfo5iv6OL_GIf1X5p4s&usqp=CAU",
+    author: "Marcos - Empresario",
+    comment:
+      "Magna sit amet purus gravida. Sit amet porttitor eget dolor morbi nonLectus vestibulum mattis ullamcorper velit sed ullamcorper.Diam phasellus .",
+  },
+];
