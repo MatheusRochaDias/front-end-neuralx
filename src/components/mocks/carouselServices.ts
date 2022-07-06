ca7750d38c71a51316feb22d73909fcf6ef018bd
Allisson-Lima-dev <Allysoncosta@outlook.com>
2022-07-06 17:50:50 -0300

    feat: adicionando mock para servicos no caousel

---
+++ src/components/mocks/carouselServices.ts
@@ -0,0 +1,24 @@
+import Ia from "../../../public/assets/IA.png";
+import Img from "../../../public/assets/imgCarousel.png";
+
+export const data = [
+  {
+    image: Img.src,
+    title: "Visão Computacional",
+    description:
+      "Id consectetur purus ut faucibus pulvinar elementum integer enim. A arcu cursus vitae congue mauris rhoncus aeneanconsectetur purus ut faucibus pulvinar elementum integer enim. A arcu cursus vitae congue mauris rhoncus aenean",
+  },
+  {
+    image: Ia.src,
+    title: "Deep learning",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ad tempore consequuntur nihil reprehenderit aliquam minus quidem at maxime dolorum voluptatibus, eum suscipit minima natus. Porro pariatur aliquam quam velit.",
+  },
+  {
+    image:
+      "https://www.totvs.com/wp-content/uploads/2019/06/o-que-e-inteligencia-artificial.jpg",
+    title: "Inteligencia Artificial",
+    description:
+      "Magna sit amet purus gravida. Sit amet porttitor eget dolor morbi nonLectus vestibulum mattis ullamcorper velit sed ullamcorper.Diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Dui faucibus in ornare quam viverra.Posuere ac consequat semper viverra.",
+  },
+];
